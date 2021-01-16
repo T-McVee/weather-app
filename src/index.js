84da903d9dda0066dad517b38251705f97fe42d5
T-McVee <iam@tmcvee.com>
2021-01-16 12:40:25 -0800

    add bootstrap

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './css/main.css';
+import { Tooltip, Toast, Popover } from 'bootstrap';
+
+const content = document.querySelector('#content');
+
+const containerOne = document.createElement('div');
+containerOne.classList.add('container');
+
+content.appendChild(containerOne);
--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,36 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            // translates CSS into CommonJS modules
+            loader: 'css-loader',
+          },
+          {
+            // Run postcss actions
+            loader: 'postcss-loader',
+            options: {
+              // `postcssOptions` is needed for postcss 8.x;
+              // if you use postcss 7.x skip the key
+              postcssOptions: {
+                // postcss plugins, can be exported to postcss.config.js
+                plugins: () => {
+                  return [require('autoprefixer')];
+                },
+              },
+            },
+          },
+          {
+            // compiles Sass to CSS
+            loader: 'sass-loader',
+          },
+        ],
+      },
     ],
   },
   output: {
